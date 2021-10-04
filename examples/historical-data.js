9b2720bc4a23a716d22cae8644db12e6fd5dd704
loloToster <lolo.toster4@gmail.com>
2021-10-04 22:17:28 +0200

    added historical-data example

---
+++ examples/historical-data.js
@@ -0,0 +1,19 @@
+const OpenWeatherAPI = require("../index")
+
+// Set global key, location and units
+let weather = new OpenWeatherAPI({
+    key: "put-key-here",
+    coordinates: {
+        lat: 49.84,
+        lon: 24.03
+    }
+})
+
+// getHistory is asynchronous so we can use .then()
+// we pass a timestamp of the time that was 15 minutes ago
+weather.getHistory(new Date().getTime() - 900000).then(async data => {
+    let time = data.current.dt
+    let location = await weather.getLocation()
+    // and we get temperature from 15 minutes ago
+    console.log(`The temperature at ${time.getHours()}:${time.getMinutes()} in ${location.name} was ${data.current.weather.temp.cur}\u00B0K`)
+})
