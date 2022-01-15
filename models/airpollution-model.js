06fc7658b057fe7df8e3f52713ced95f32e097f3
loloToster <lolo.toster4@gmail.com>
2022-01-15 20:06:27 +0100

    📝 added jsdoc to models

--- models/airpollution-model.js
+++ models/airpollution-model.js
@@ -1,22 +1,48 @@
 const airPollutionModel = {
-    lat: undefined, // Geographical coordinates of the location (latitude)
-    lon: undefined, // Geographical coordinates of the location (longitude)
-    dt: undefined,  // Date and time
-    dt_raw: undefined, // Date and time, Unix, UTC
-    aqi: undefined, // Air Quality Index.
-    aqi_name: undefined,// String substitute of aqi field (only english)
+    lat: undefined,
+    lon: undefined,
+    dt: undefined,
+    dt_raw: undefined,
+    aqi: undefined,
+    aqi_name: undefined,
     components: {
-        co: undefined, // Сoncentration of CO (Carbon monoxide), μg/m3
-        no: undefined, // Сoncentration of NO (Nitrogen monoxide), μg/m3
-        no2: undefined, // Сoncentration of NO2 (Nitrogen dioxide), μg/m3
-        o3: undefined, // Сoncentration of O3 (Ozone), μg/m3
-        so2: undefined, // Сoncentration of SO2 (Sulphur dioxide), μg/m3
-        pm2_5: undefined, // Сoncentration of PM2.5 (Fine particles matter), μg/m3
-        pm10: undefined, // Сoncentration of PM10 (Coarse particulate matter), μg/m3
-        nh3: undefined // Сoncentration of NH3 (Ammonia), μg/m3
+        co: undefined,
+        no: undefined,
+        no2: undefined,
+        o3: undefined,
+        so2: undefined,
+        pm2_5: undefined,
+        pm10: undefined,
+        nh3: undefined
     }
 }
 
+/**
+ * @typedef {Object} Components
+ * @property {Number} co Сoncentration of CO (Carbon monoxide), μg/m3
+ * @property {Number} no Сoncentration of NO (Nitrogen monoxide), μg/m3
+ * @property {Number} no2 Сoncentration of NO2 (Nitrogen dioxide), μg/m3
+ * @property {Number} o3 Сoncentration of O3 (Ozone), μg/m3
+ * @property {Number} so2 Сoncentration of SO2 (Sulphur dioxide), μg/m3
+ * @property {Number} pm2_5 Сoncentration of PM2.5 (Fine particles matter), μg/m3
+ * @property {Number} pm10 Сoncentration of PM10 (Coarse particulate matter), μg/m3
+ * @property {Number} nh3 Сoncentration of NH3 (Ammonia), μg/m3
+ */
+
+/**
+ * @typedef {Object} AirPollution
+ * @property {Number} lat Geographical coordinates of the location (latitude)
+ * @property {Number} lon Geographical coordinates of the location (longitude)
+ * @property {Date} dt Date and time, UTC
+ * @property {Number} dt_raw Date and time, Unix, UTC
+ * @property {Number} aqi Air Quality Index.
+ * @property {String} aqi_name String substitute of aqi field (only english)
+ * @property {Components} components
+ */
+
+/**
+ * @returns {AirPollution}
+ */
 function getAirPollutionModel() {
     return JSON.parse(JSON.stringify(airPollutionModel))
 }
--- models/weather-model.js
+++ models/weather-model.js
@@ -1,62 +1,143 @@
 const weatherModel = {
-    lat: undefined, // Geographical coordinates of the location (latitude)
-    lon: undefined, // Geographical coordinates of the location (longitude)
-    dt: undefined,  // Timezone name for the requested location
+    lat: undefined,
+    lon: undefined,
+    dt: undefined,
     dt_raw: undefined,
-    timezone: undefined, // Shift in seconds from UTC
-    timezone_offset: undefined, // Shift in seconds from UTC
+    timezone: undefined,
+    timezone_offset: undefined,
     astronomical: {
-        sunrise: undefined, // Sunrise time, Unix, UTC
+        sunrise: undefined,
         sunrise_raw: undefined,
-        sunset: undefined, // Sunset time, Unix, UTC
+        sunset: undefined,
         sunset_raw: undefined,
-        moonrise: undefined, // The time of when the moon rises for this day, Unix, UTC
+        moonrise: undefined,
         moonrise_raw: undefined,
-        moonset: undefined, // The time of when the moon sets for this day, Unix, UTC
+        moonset: undefined,
         moonset_raw: undefined,
-        moon_phase: undefined // Moon phase. 0 and 1 are 'new moon', 0.25 is 'first quarter moon', 0.5 is 'full moon' and 0.75 is 'last quarter moon'. The periods in between are called 'waxing crescent', 'waxing gibous', 'waning gibous', and 'waning crescent', respectively.
+        moon_phase: undefined
     },
     weather: {
-        temp: { // Units – default: kelvin, metric: Celsius, imperial: Fahrenheit.
-            cur: undefined, // Current temperature. Units - default: kelvin, metric: Celsius, imperial: Fahrenheit.
-            morn: undefined, // Morning temperature.
-            day: undefined, // Day temperature.
-            eve: undefined, // Evening temperature.
-            night: undefined, // Night temperature.
-            min: undefined, // Lowest daily temperature.
-            max: undefined // Highest daily temperature.
+        temp: {
+            cur: undefined,
+            morn: undefined,
+            day: undefined,
+            eve: undefined,
+            night: undefined,
+            min: undefined,
+            max: undefined
         },
-        feels_like: { // This accounts for the human perception of weather. Units – default: kelvin, metric: Celsius, imperial: Fahrenheit.
-            cur: undefined, // Current temperature. Units - default: kelvin, metric: Celsius, imperial: Fahrenheit.
-            morn: undefined, // Morning temperature.
-            day: undefined, // Day temperature.
-            eve: undefined, // Evening temperature.
-            night: undefined // Night temperature.
+        feels_like: {
+            cur: undefined,
+            morn: undefined,
+            day: undefined,
+            eve: undefined,
+            night: undefined
         },
-        pressure: undefined, // Atmospheric pressure on the sea level, hPa
-        humidity: undefined, // Humidity, %
-        dew_point: undefined, // Atmospheric temperature (varying according to pressure and humidity) below which water droplets begin to condense and dew can form. Units – default: kelvin, metric: Celsius, imperial: Fahrenheit.
-        clouds: undefined, // Cloudiness, %
-        uvi: undefined, // The maximum value of UV index for the day
-        visibility: undefined, // Average visibility, metres
+        pressure: undefined,
+        humidity: undefined,
+        dew_point: undefined,
+        clouds: undefined,
+        uvi: undefined,
+        visibility: undefined,
         wind: {
-            speed: undefined, // Wind speed. Units – default: metre/sec, metric: metre/sec, imperial: miles/hour.
-            gust: undefined, // Wind gust. Units – default: metre/sec, metric: metre/sec, imperial: miles/hour.
-            deg: undefined // Wind direction, degrees (meteorological)
+            speed: undefined,
+            gust: undefined,
+            deg: undefined
         },
-        pop: undefined, // Probability of precipitation
-        rain: undefined, // Precipitation volume, mm
-        snow: undefined, // Snow volume, mm
-        condition_id: undefined, // Weather condition id (https://openweathermap.org/weather-conditions#Weather-Condition-Codes-2)
-        main: undefined, // Group of weather parameters (Rain, Snow, Extreme etc.)
-        description: undefined, // Description of the weather
+        pop: undefined,
+        rain: undefined,
+        snow: undefined,
+        condition_id: undefined,
+        main: undefined,
+        description: undefined,
         icon: {
-            url: undefined, // Weather icon url.
-            raw: undefined  // Weather icon id.
+            url: undefined,
+            raw: undefined
         }
     }
 }
 
+/**
+ * @typedef {Object} Astronomical
+ * @property {Date} sunrise Sunrise time, Unix, UTC
+ * @property {Number} sunrise_raw
+ * @property {Date} sunset Sunset time, Unix, UTC
+ * @property {Number} sunset_raw
+ * @property {Date} moonrise The time of when the moon rises for this day, Unix, UTC
+ * @property {Number} moonrise_raw
+ * @property {Date} moonset The time of when the moon sets for this day, Unix, UTC
+ * @property {Number} moonset_raw
+ * @property {Number} moon_phase Moon phase. 0 and 1 are 'new moon', 0.25 is 'first quarter moon', 0.5 is 'full moon' and 0.75 is 'last quarter moon'. The periods in between are called 'waxing crescent', 'waxing gibous', 'waning gibous', and 'waning crescent', respectively.
+ */
+
+/**
+ * @typedef {Object} Temperatures
+ * @property {Number} cur Current temperature or estimated temperature (in hourly forecast)
+ * @property {Number} morn Morning temperature
+ * @property {Number} day Day temperature
+ * @property {Number} eve Evening temperature
+ * @property {Number} night Night temperature
+ * @property {Number} min Lowest daily temperature
+ * @property {Number} max Highest daily temperature
+ */
+
+/**
+ * @typedef {Object} FeelsLike
+ * @property {Number} cur Current temperature or estimated temperature (in hourly forecast)
+ * @property {Number} morn Morning temperature
+ * @property {Number} day Day temperature
+ * @property {Number} eve Evening temperature
+ * @property {Number} night Night temperature
+ */
+
+/**
+ * @typedef {Object} WindData
+ * @property {Number} speed Wind speed
+ * @property {Number} gust Wind gust
+ * @property {Number} deg Wind direction, degrees (meteorological)
+ */
+
+/**
+ * @typedef {Object} Icon
+ * @property {String} url Weather icon url
+ * @property {String} raw Weather icon id
+ */
+
+/**
+ * @typedef {Object} Conditions
+ * @property {Temperatures} temp Units – default: kelvin, metric: Celsius, imperial: Fahrenheit.
+ * @property {FeelsLike} feels_like This accounts for the human perception of weather. Units – default: kelvin, metric: Celsius, imperial: Fahrenheit.
+ * @property {Number} pressure Atmospheric pressure on the sea level, hPa
+ * @property {Number} humidity Humidity, %
+ * @property {Number} dew_point Atmospheric temperature (varying according to pressure and humidity) below which water droplets begin to condense and dew can form. Units – default: kelvin, metric: Celsius, imperial: Fahrenheit.
+ * @property {Number} clouds Cloudiness, %
+ * @property {Number} uvi The maximum value of UV index for the day
+ * @property {Number} visibility Average visibility, metres
+ * @property {WindData} wind Wind statistics. Units – default: metre/sec, metric: metre/sec, imperial: miles/hour.
+ * @property {Number} pop Probability of precipitation
+ * @property {Number} rain Precipitation volume, mm
+ * @property {Number} snow Snow volume, mm
+ * @property {Number} condition_id Weather condition id (https://openweathermap.org/weather-conditions#Weather-Condition-Codes-2)
+ * @property {String} main Group of weather parameters (Rain, Snow, Extreme etc.)
+ * @property {String} description Description of the weather
+ * @property {Icon} icon
+ */
+
+/**
+ * @typedef {Object} Weather
+ * @property {Number} lat Geographical coordinates of the location (latitude)
+ * @property {Number} lon Geographical coordinates of the location (longitude)
+ * @property {Date} dt Date and time, UTC
+ * @property {Number} dt_raw Date and time, Unix, UTC
+ * @property {String} timezone Date and time, Unix, UTC
+ * @property {Number} timezone_offset Date and time, Unix, UTC
+ * @property {Astronomical} astronomical
+ * @property {Conditions} weather
+ */
+
+/**
+ * @returns {Weather}
+ */
 function getWeatherModel() {
     return JSON.parse(JSON.stringify(weatherModel))
 }
